b735e3f29c818b8bfac29d131251e9c6f5ac72bc
Khan, Asfand (k1802418) <asfand987.ak@kcl.ac.uk>
2019-11-19 15:35:15 +0000

    Kanban Column

--- Kanban/src/main/java/KanbanBoard/Kanban.java
+++ Kanban/src/main/java/KanbanBoard/Kanban.java
@@ -1,19 +1,23 @@
 package KanbanBoard;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 
 import javafx.scene.control.Label;
-
-import java.awt.*;
+import javafx.event.ActionEvent;
+//import java.awt.*;
+//import java.awt.event.ActionEvent;
 import java.io.IOException;
 
 public class Kanban extends Application {
+    @FXML private Button closeButton;
     public static void main(String[] args) {
         launch(args);
     }
@@ -26,8 +30,10 @@ public class Kanban extends Application {
         } catch (IOException e){
             throw new RuntimeException(e);
         }
+
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+
 }
