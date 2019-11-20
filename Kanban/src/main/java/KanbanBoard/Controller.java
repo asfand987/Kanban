896b71b166731c4f213fa343791000eef96b3d65
Khan, Asfand (k1802418) <asfand987.ak@kcl.ac.uk>
2019-11-20 14:00:35 +0000

    Controller class

---
+++ Kanban/src/main/java/KanbanBoard/Controller.java
@@ -0,0 +1,59 @@
+package KanbanBoard;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
+public class Controller
+{
+    //@FXML
+    // The reference of inputText will be injected by the FXML loader
+    @FXML public AnchorPane card1;
+    @FXML public GridPane gridPane;
+    @FXML public GridPane column1;
+    //@FXML public VBox v1;
+    @FXML
+    private void initialize()
+    {
+    }
+
+    @FXML
+    public void detect() {
+        card1.setOnDragDetected((MouseEvent event) -> {
+            Dragboard db = card1.startDragAndDrop(TransferMode.MOVE);
+            ClipboardContent content = new ClipboardContent();
+            content.putString(card1.getId());
+            db.setContent(content);
+            event.consume();
+        });
+    }
+
+    @FXML
+    public void accept() {
+        gridPane.addEventHandler(DragEvent.DRAG_OVER, (DragEvent event) -> {
+            if (event.getGestureSource() != gridPane
+                    && event.getDragboard().hasString()) {
+                event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+            }
+            event.consume();
+        });
+    }
+
+    @FXML
+    public void drop() {
+        gridPane.addEventHandler(DragEvent.DRAG_DROPPED, (DragEvent event) -> {
+            Dragboard db = event.getDragboard();
+            boolean success = false;
+            if (db.hasString()) {
+                gridPane.getChildren().add(card1);
+
+                success = true;
+            }
+            event.setDropCompleted(success);
+            event.consume();
+        });
+    }
+}
--- Kanban/src/main/java/KanbanBoard/Kanban.java
+++ Kanban/src/main/java/KanbanBoard/Kanban.java
@@ -5,19 +5,12 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.*;
 import javafx.stage.Stage;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
-
-import javafx.scene.control.Label;
-import javafx.event.ActionEvent;
-//import java.awt.*;
-//import java.awt.event.ActionEvent;
 import java.io.IOException;
 
 public class Kanban extends Application {
-    @FXML private Button closeButton;
+    //private Button b;
     public static void main(String[] args) {
         launch(args);
     }
@@ -27,13 +20,16 @@ public class Kanban extends Application {
         Parent root = null;
         try {
             root = FXMLLoader.load(getClass().getResource("Kanban.fxml"));
-        } catch (IOException e){
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        Controller controller = new Controller();
+        FXMLLoader loader = new FXMLLoader();
+        loader.setController(controller);
+        //Controller c = loader.getController();
 
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
 }
