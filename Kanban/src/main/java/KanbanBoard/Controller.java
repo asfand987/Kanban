7e7443f2ea43552c5a6b5d3829277c58cb7c4203
asfand987 <48281686+asfand987@users.noreply.github.com>
2019-12-02 15:38:40 +0000

    update controller

--- Kanban/src/main/java/KanbanBoard/Controller.java
+++ Kanban/src/main/java/KanbanBoard/Controller.java
@@ -1,55 +1,116 @@
 package KanbanBoard;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.input.*;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
-public class Controller
-{
-    @FXML public AnchorPane card1;
-    @FXML public GridPane gridPane;
-    @FXML public GridPane column1;
+
+import java.util.ArrayList;
+
+public class Controller {
+    @FXML
+    private Button Log;
+    @FXML
+    private Button card;
+    @FXML
+    private Button delete;
+    @FXML
+    private TextField newCol;
+    @FXML
+    private TextField newCard;
+    @FXML
+    private TextField colTitle;
+    @FXML
+    private TextField boardTitle;
+    @FXML
+    private VBox row;
+    @FXML
+    private HBox col;
 
     @FXML
+    ArrayList<Button> inner = new ArrayList<Button>();
+    @FXML
+    private ArrayList<Button> cardCount;
+
+
     private void initialize()
-    {}
+    {
+    }
+
+
 
     @FXML
-    public void detect() {
-        card1.setOnDragDetected((MouseEvent event) -> {
-            Dragboard db = card1.startDragAndDrop(TransferMode.MOVE);
-            ClipboardContent content = new ClipboardContent();
-            content.putString(card1.getId());
-            db.setContent(content);
-            event.consume();
+    private void addNewCol(String title){
+        // int count = 0;
+        //outer.add(inner);
+    }
+    @FXML
+    private void addNewCard(String name)
+    {
+        int count = 0;
+        inner.add(count,card);
+        card.setText(name);
+    }
+
+    @FXML
+    private void addCardPress(){
+
+        newCard.setOnAction((event)->{
+
+            Button but = new Button();
+            but.setPrefWidth(160);
+            but.setText(newCard.getText());
+            row.getChildren().add(but);
+            but.setOnMouseDragged(new EventHandler<MouseEvent>()
+            {
+                public void handle(MouseEvent event)
+                {
+                    System.out.println("Event on Source: mouse dragged");
+                }
+            });
+            delete.setOnMouseDragOver(new EventHandler <MouseDragEvent>()
+            {
+                public void handle(MouseDragEvent event)
+                {
+                    //row.getChildren().removeAll();
+                    System.out.println("Button dragged to the delete button");
+                }
+            });
         });
     }
 
     @FXML
-    public void accept() {
-        gridPane.addEventHandler(DragEvent.DRAG_OVER, (DragEvent event) -> {
-            if (event.getGestureSource() != gridPane
-                    && event.getDragboard().hasString()) {
-                event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+    private void addColPress(){
+        Button title = new Button();
+        title.setPrefWidth(160);
+        newCol.setOnAction((event)->{
+            title.setText(newCol.getText());
+            col.getChildren().add(title);
+        });
+    }
+
+
+    /*EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e)
+            {
+                l.setText(b.getText());
             }
-            event.consume();
-        });
-    }
+        };
 
-    @FXML
-    public void drop() {
-        gridPane.addEventHandler(DragEvent.DRAG_DROPPED, (DragEvent event) -> {
-            Dragboard db = event.getDragboard();
-            boolean success = false;
-            if (db.hasString()) {
-                gridPane.getChildren().add(card1);
+        // when enter is pressed
+        b.setOnAction(event);
 
-                success = true;
-            }
-            event.setDropCompleted(success);
-            event.consume();
-        });
-    }
+        // add textfield
+        r.getChildren().add(b);
+        r.getChildren().add(l);*/
 }
+
+
